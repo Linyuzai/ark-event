a97fe074ff0f6ea16c032925d1f51fdab39b997e
thz <t88510930>
2021-03-01 11:31:56 +0800

    补充方法

--- ark-event-core/src/main/java/com/github/linyuzai/arkevent/core/ArkEvent.java
+++ ark-event-core/src/main/java/com/github/linyuzai/arkevent/core/ArkEvent.java
@@ -2,6 +2,7 @@ package com.github.linyuzai.arkevent.core;
 
 import com.github.linyuzai.arkevent.support.ArkEventPlugin;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public interface ArkEvent {
@@ -12,5 +13,16 @@ public interface ArkEvent {
 
     default void publish(Map<Object, Object> args) {
         ArkEventPlugin.getPublisher().publish(this, args);
+    }
+
+    @SuppressWarnings("unchecked")
+    default void publish(Object args) {
+        if (args instanceof Map) {
+            publish((Map<Object, Object>) args);
+        } else {
+            Map<Object, Object> map = new HashMap<>();
+            map.put(args, args);
+            publish(map);
+        }
     }
 }
